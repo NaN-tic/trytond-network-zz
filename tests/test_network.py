32975b9f1b3445b9fe3e9a079a66c1facf80f50c
resteve <resteve@zikzakmedia.com>
2015-12-01 11:10:49 +0100

    Tests

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of network module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the network module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class NetworkTestCase(unittest.TestCase):
+class NetworkTestCase(ModuleTestCase):
     'Test Network module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('network')
-
-    def test0005views(self):
-        'Test views'
-        test_view('network')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'network'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         NetworkTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
